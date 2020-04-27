28c5ac1c80f7244d87a09a89850986b4df0c56f0
akhiltn <tnakhil@gmail.com>
2020-04-27 21:36:30 +0530

    master commit

---
+++ data/data.js
@@ -0,0 +1,69 @@
+let profile = {
+  picture: "https://lh3.googleusercontent.com/kCqxOclwU9CdYPPXoBM7iLssOgQWMftae15roU_Cq3W9ukVzVxFKUqB69RqJWtrr5np5jVDNvI0EmYNdw31rFFihv4iX5H4Xy7-Jz5_coaPdFUgq7X_KE1Q5iBmoSY5S2ot2JeTo0Ik=w2400",
+  name: "Akhil T Narendran",
+  description: "Software Engineer & Fullstack Developer",
+  bio:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur neque arcu, et sagittis ex eleifend at. Vestibulum consectetur in eros eget faucibus. Sed euismod in nisl a varius. Cras blandit dictum porttitor. Vestibulum vitae lacus euismod, condimentum magna vel, vulputate lectus. Etiam fermentum velit eu neque tincidunt semper. Integer id erat vitae magna tempor consectetur. Sed sed ultricies diam. Mauris ut eros tristique, efficitur sapien luctus, egestas erat. Curabitur maximus lacus dui, eget consequat nisl rhoncus et. Aenean posuere tortor nec justo finibus maximus. Pellentesque sollicitudin faucibus neque vel gravida. Praesent lobortis, lacus ut maximus sollicitudin, mauris sem rutrum nulla, laoreet vehicula nulla erat non massa.",
+  email: "guim.gonzalez@gmail.com",
+  location: "Bangalore, India",
+  birth: "1991-04-20", // YYYY-MM-DD format
+  skills: [
+    "Java",
+    "CSS",
+    "HTML",
+    "Javascript",
+    "TypeScript",
+    "Python",
+    "Angular"
+  ],
+  social: {
+    linkedin: "https://example.com/",
+    twitter: "https://example.com/",
+    github: "https://example.com/",
+    stackoverflow: "https://example.com/",
+    dev: "https://example.com/"
+  },
+  experience: [
+    {
+      title: "Job title 2",
+      company: "Company 2",
+      url: "https://example.com/",
+      location: "Barcelona, Spain",
+      description: "Description of the job",
+      from: "September 2017",
+      to: "Now"
+    },
+    {
+      title: "Job title 1",
+      company: "Company 1",
+      url: "https://example.com/",
+      location: "Barcelona, Spain",
+      description: "Description of the job",
+      from: "July 2018",
+      to: "February 2019"
+    }
+  ],
+  education: [
+    {
+      school: "Universitat Autònoma de Barcelona",
+      degree: "Engineering",
+      fieldOfStudy: "Computer Science",
+      location: "Barcelona, Spain",
+      from: "September 2016",
+      to: "Now"
+    }
+  ],
+  quote: "Experianced JAVA Fullstack Developer"
+};
+
+let page = {
+  language: "english",
+  // ^ languages suported:
+  //  - english (default)
+  theme: "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+};
